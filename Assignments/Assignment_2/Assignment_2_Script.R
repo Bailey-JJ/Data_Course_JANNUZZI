b0a677d33cf8f9f40e9d5d00eef9eec25cf352bd
Bailey-JJ <bailey.jannuzzi@gmail.com>
2024-01-16 11:34:18 -0700

    1st commit

---
+++ Assignments/Assignment_2/Assignment_2_Script.R
@@ -0,0 +1,25 @@
+#Assignment 2 (B. Jannuzzi)
+
+#Task 4: Find the csv files and save as object
+csv_files <- list.files(path = "Data", pattern = ".csv", recursive = TRUE)
+
+#Task 5: Find how many files match description with length function
+length(csv_files)
+
+#Task 6: Open the wingspan_vs_mass
+df <- read.csv(list.files(pattern = "wingspan_vs_mass.csv", recursive = TRUE))
+
+#Task 7: Inspect the first 5 lines of the data
+head(df, 5)
+
+#Task 8: Find any files (recursively) in the Data/directory that begin with the letter "b".
+list.files(path = "Data", pattern = "^b", recursive = TRUE)
+
+#Task 9: Write a command that displays the first line of each of those “b” files
+bfiles <- list.files(path = "Data", pattern = "^b", recursive = TRUE, full.names = TRUE)
+
+for(i in bfiles){
+print(readLines(i, n = 1))
+}
+
+#Task 10:
